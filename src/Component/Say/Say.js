ecd8b1108c02162d2e29c8421a2aae9af63878ca
EZ <panzhengpan@163.com>
2018-08-20 15:19:48 +0800

    添加了一个说说界面

--- src/Component/Say/Say.js
+++ src/Component/Say/Say.js
@@ -9,16 +9,16 @@ class Say extends PureComponent {
         const Item = Timeline.Item;
         return (
             <div className={'sayContainer'}>
-                <Timeline>
+                <Timeline pending={'还在添砖加瓦中'}>
                     {
                         [1, 2, 3].map((item, index) => {
                             return (
                                 <Item key={index}>
                                     <div className='item'>
                                         <div className="content">
-                                            <p>ergragreag个人隔热管他人给他任何一天他任何一天就会有同居个人隔热管他人给他任何一天就会有同居也可以看看已经提交人ergragreag个人隔热管他人给他任何一天就会有同居也可以看看已经提交人</p>
+                                            <p>这是一个使用react构建的小页面，有时会随便添加点小东西</p>
                                         </div>
-                                        <span>发表于： ‘司法所的范德萨发的身份问题’</span>
+                                        <span>发表于： XXX-XXX-XXX</span>
                                     </div>
                                 </Item>
                             );
